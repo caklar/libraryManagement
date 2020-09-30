3ffd1c883f2f1928c76d401678a145746576bfd2
caklar <acuihz123456@gmail.com>
2020-09-30 10:40:38 +0800

    主页组件

---
+++ src/components/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="frame">
+        <header class="header">
+            <h1 class="sign">欢迎使用图书管理系统</h1>
+            <div class="user">123</div>
+        </header>
+        <div class="content">
+            <router-view></router-view>
+        </div>
+        <aside class="aside-nav">
+            <a href="">首页</a>
+            <router-link to="/books">图书管理</router-link>
+            <router-link to="/readers">读者管理</router-link>
+            <router-link to="/borrow">借阅管理</router-link>
+            <a href="">数据统计</a>
+        </aside>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
