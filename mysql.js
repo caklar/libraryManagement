2c07b4b80cc80be0e0baa8ac91ce54c950ba7699
caklar <acuihz123456@gmail.com>
2020-10-11 22:31:04 +0800

    数据库连接模块

---
+++ mysql.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql')
+
+// 创建数据库连接
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '123698745',
+    database: 'library',
+    multipleStatements: true
+})
+
+// 操作
+var query = function (sql, data, callback) {
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            callback(err)
+        } else {
+            connection.query(sql, data, function (qerr, back) {
+                // 事件驱动回调
+                callback(qerr, back)
+            });
+        }
+        // 释放连接
+        pool.releaseConnection(connection)
+    });
+};
+
+module.exports = query;
