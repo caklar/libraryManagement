33f8c0ab6f63069ff34511706a2956cc2126040a
caklar <acuihz123456@gmail.com>
2020-10-11 22:32:16 +0800

    图书详细信息组件

---
+++ src/components/BookDetail.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bookDetail">
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['bookDetail']
+    }
+</script>
+
+<style scoped>
+
+</style>
