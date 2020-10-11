e738f8463364cf4dea4a146babb2818d53554ac8
caklar <acuihz123456@gmail.com>
2020-10-11 22:34:38 +0800

    读者管理组件

--- src/components/readers.vue
+++ src/components/readers.vue
@@ -1,40 +1,126 @@
 <template>
 	<div class="readers">
         <div class="readers-control">
-            <a href="">信息录入</a>
-            <a href="">信息修改</a>
-            <a href="">借阅记录</a>
-            <input type="text" class="readers-search" placeholder="输入读者编号、姓名或证件号">
+            <router-link to='/readers/newReader' @click.native="readerCR = false">信息录入</router-link>
+            <a href="javascript:;" @click="reviseReader">信息修改</a>
+            <!-- <router-link :to='{ name:"reviseReader", params:{ id: readerId } }' @click.native="getReaderDetail">信息修改</router-link> -->
+            <router-link to='/readers' @click.native="getReaders">信息查看</router-link>
+            <a href="javascript:;" @click="getReaderBorrow">借阅记录</a>
+            <!-- <router-link :to='{ name:"readerBorrow", params: { id: readerId } }' @click.native="getReaderBorrow">借阅记录</router-link> -->
+            <input type="text" class="readers-search" placeholder="输入读者编号、姓名或证件号" v-model="searchMsg" @keydown="getSearchReader($event)">
         </div>
-        <div class="readers-msg">
-            <table>
-                <thead>
-                    <tr>
-                        <td>编号</td>
-                        <td>姓名</td>
-                        <td>性别</td>
-                        <td>证件号</td>
-                        <td>联系方式</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <router-view :readerMsg='readerMsg' :borrowMsg="readerBorrow" :readerDetail="readerDetail" :readerCR="readerCR" @getReaderId="getReaderId"></router-view>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
+
+    export default {
+        mounted () {
+            this.getReaders()
+        },
+        data () {
+            return {
+                readerMsg: [],
+                readerId: '',
+                readerBorrow: [],
+                readerDetail: [],
+                readerCR: false,
+                searchMsg: ''
+            }
+        },
+        methods: {
+            // 获取全部读者信息
+            getReaders () {
+                axios.get('/readers').then(res =>
+                    this.readerMsg = res.data
+                )
+            },
+
+            // 获取子组件传递过来的读者编号
+            getReaderId (data) {
+                this.readerId = data
+            },
+
+            // 根据读者编号获取读者的借阅信息
+            getReaderBorrow () {
+                if (this.readerId) {
+                    // 获取到读者 id 后获得改读者的借阅数据
+                    axios.get('/readerBorrow', {
+                        params: {
+                            readerId: this.readerId
+                        }
+                    }).then(res => {
+                        this.readerBorrow = res.data
+                        // 路由跳转并传参
+                        this.$router.push({
+                            name: 'readerBorrow',
+                            params: {
+                                id: this.readerId
+                            }
+                        })
+                    })
+                } else {
+                    alert('请选择读者')
+                }
+            },
+
+            // 根据读者编号获取读者信息并跳转到修改页面
+            reviseReader () {
+                if (this.readerId) {
+                    // 获取到读者 id 后获得改读者的信息
+                    axios.get('/getReaderDetail', {
+                        params: {
+                            readerId: this.readerId
+                        }
+                    }).then(res => {
+                        this.readerDetail = res.data
+                        // 路由跳转并传参
+                        this.$router.push({
+                            name: 'reviseReader',
+                            params: {
+                                id: this.readerId
+                            }
+                        })
+                    })
+                    // 改变标志用来判断为修改页面
+                    this.readerCR = true
+                } else {
+                    alert('请选择读者')
+                }
+            },
+
+            // 获取搜索的读者数据
+            getSearchReader (e) {
+                if (e.keyCode == 13) {
+                    // 按下回车时根据 input 中的内容查找数据
+                    axios.get('/searchReader', {
+                        params: {
+                            searchMsg: this.searchMsg
+                        }
+                    }).then(res => {
+                        // 判断结果是否为空
+                        if (Number(res.data) != 0) {
+                            this.readerMsg = res.data
+                            // 路由跳转
+                            this.$router.push({
+                                name: 'searchReader',
+                                params: {
+                                    msg: this.searchMsg
+                                }
+                            })
+                        } else {
+                            alert('未查找到数据')
+                        }
+                    })
+                }
+            }
+        }
+    }
 </script>
 
-<style>
+<style scope>
     .readers {
         height: 100%;
     }
@@ -47,7 +133,7 @@
     }
 
     .readers-control a:nth-child(1) {
-        margin-left: 164px;
+        margin-left: 132px;
     }
 
     .readers-control a {
@@ -63,8 +149,7 @@
     }
 
     .readers-control a:hover {
-        background: #888;
-        color: #fff;
+        background-color: #e3e3e3;
     }
 
     .readers-search {
@@ -78,20 +163,18 @@
         border: 0;
         border-bottom: 1px solid #e3e3e3;
         font-size: 16px;
+        transition: border-bottom 0.1s linear;
     }
 
-    .readers-msg {
+    .readers-search:focus {
+        border-bottom: 1px solid #555;
+    }
+
+    .readers-content {
         height: 615px;
     }
 
-    .readers-msg table {
+    .readers-content table {
         width: 100%;
     }
-
-    /* .readers-msg thead td {
-        height: 35px;
-        border:1px solid #d3d3d3;
-        background: #f1f1f1;
-        text-align: center;
-    } */
 </style>
